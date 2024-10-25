e32b0a4b388bb40803b9472cdbb5cdbe39cefe67
Deák Zsombor <deak.zsombor@ckik.loc>
2024-10-25 11:12:56 +0200

    2024.10.25

---
+++ 2024_10_25 Péntek Osztályos feladatok/Haromszog_osztaly/Haromszog_osztaly/Haromszog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haromszog_osztaly
+{
+    class Haromszog
+    {
+        public double oldal1, oldal2, oldal3;
+
+        public Haromszog(string sor)
+        {
+            string[] st = sor.Split(';');
+            this.oldal1 = Convert.ToDouble(st[0]);
+            this.oldal2 = Convert.ToDouble(st[1]);
+            this.oldal3 = Convert.ToDouble(st[2]);
+        }
+
+        public bool SzerkeszthetoE()
+        {
+            return oldal1 + oldal2 > oldal3 && oldal1 + oldal3 > oldal2 && oldal2 + oldal3 > oldal1;
+        }
+
+        public bool EgyenloSzaruE()
+        {
+            return oldal1 == oldal2 || oldal2 == oldal3 || oldal3 == oldal1;
+        }
+    }
+}
---
+++ 2024_10_25 Péntek Osztályos feladatok/Haromszog_osztaly/Haromszog_osztaly/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Haromszog_osztaly
+{
+    internal class Program
+    {
+        
+        static List<Haromszog> haromszogek = new List<Haromszog>();
+
+        static void Main(string[] args)
+        {
+            Fajlbeolvasas();
+            Feladat1();
+
+            Console.ReadLine();
+        }
+
+        static void Feladat1()
+        {
+            int db = SzerkeszthetoHaromszogekDB();
+            Console.WriteLine($"{db} db szerkeszthető háromszögvan.");
+        }
+
+        static int SzerkeszthetoHaromszogekDB()
+        {
+            int db = 0;
+            for (int i = 0; i < haromszogek.Count; i++)
+                if (haromszogek[i].SzerkeszthetoE())
+                    db++;
+            return db;
+        }
+
+
+        static void Fajlbeolvasas()
+        {
+            StreamReader f = new StreamReader("haromszogek.txt");
+            f.ReadLine();
+            while (!f.EndOfStream)
+            {
+                haromszogek.Add(new Haromszog(f.ReadLine()));
+            }
+            f.Close();
+        }
+    }
+}
