a08098fc5ea821927d374c513717c6b7a674ad30
Deák Zsombor <deak.zsombor@ckik.loc>
2024-11-28 14:27:04 +0100

    2024_11_28_Absztrakt_Interface

---
+++ 2024_11_25_Interface/2024_11_25_Interface/Osztaly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_11_25_Interface
+{
+    internal class Osztaly:IFenykepezo, IDigtafon
+    {
+        public void FenykepetKeszit(int p)
+        {
+            Console.WriteLine(p + " Fényképet készítettem.");
+        }
+
+        public void Hangrogzites(string sz)
+        {
+            Console.WriteLine("Ezt a szöveget rögzítettem " + sz);
+        }
+    }
+}
