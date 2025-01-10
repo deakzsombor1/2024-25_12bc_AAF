a8abf0eb5225d61d719ce3218a26b14c18e9a9f9
Deák Zsombor <deak.zsombor@ckik.loc>
2025-01-10 11:15:34 +0100

    LINQ alapok tovabbi resze

--- 2025_01_06_LINQ_alapok/2025_01_06 LINQ_alapok/Program.cs
+++ 2025_01_06_LINQ_alapok/2025_01_06 LINQ_alapok/Program.cs
@@ -14,22 +14,97 @@ namespace _2025_01_06_LINQ_alapok
         {
             AdatokLetrehozasa();
 
-            LINQEgyszeruPelda();
-            LINQObjektumPelda();
+            //LINQEgyszeruPelda();
+            //LINQObjektumPelda();
+
+            //LINQEgyszeruPeldaEgyediElemek();
+            //LINQObjektumPeldaEgyediElemek();
+
+            //LinqAgregatumOperatorok();
+            //LinqOrderBy();
+
+            LinqEldontes();
 
             Console.ReadLine();
         }
 
+        private static void LinqEldontes()
+        {
+            //Van-e olyan, hogy ...
+            bool vaneNegativ = customers.Any(x => x.Balance < 0);
+            Console.WriteLine("Van-e olyan negativ egyenleg:" + vaneNegativ);
+        }
+
+        private static void LinqOrderBy()
+        {
+            var orderDatas = customers.OrderBy(x => x.Balance);
+
+            foreach (var elem in orderDatas)
+            {
+                Console.WriteLine(elem + " ");
+            }
+        }
+
+        private static void LinqAgregatumOperatorok()
+        {
+            //var result = (from x in customers
+            //             select x.Balance).Sum();
+
+            //var result = egeszek.Average();
+
+            //var result = customers.Sum(elem => elem.Balance);
+            //var result = customers.Average(elem => elem.Balance);
+            //var result = customers.Max(elem => elem.Balance);
+            var result = customers.Count(elem => elem.Balance>1000);
+
+            Console.WriteLine(result);
+
+        }
+
+        private static void LINQObjektumPeldaEgyediElemek()
+        {
+            //customers.Add(customers[0]);
+            //customers.Add(customers[0]);
+            //customers.Add(customers[0]);
+            var szurtElemek = customers.Select(elem=>elem.Name).Distinct();
+
+            foreach (var elem in szurtElemek)
+            {
+                Console.WriteLine(elem + " ");
+
+            }
+        }
+
+        private static void LINQEgyszeruPeldaEgyediElemek()
+        {                              
+            // Szürjük ki az egészek listából az egyedi elemeket!
+            var szurtElemek = egeszek.Distinct();
+            //HashSet - Halmaz, ahol nem lehetnek ismétlődő elemek!
+            //var szurtElemek = egeszek.ToHashSet();
+            //HashSet<int> szurtElemek = new HashSet<int> { 1,1,1,1,1,2,2,2,3,3,6,6,6,5,5};
+            foreach (var szam in szurtElemek)
+            {
+                Console.Write(szam + " ");
+            }
+        }
+
         private static void LINQObjektumPelda()
         {
-            var queryResults = from x in customers
-                               where x.AccountNumber.Length == 11
-                               select x;
+            //var queryResults = from x in customers
+            //                   where x.AccountNumber.Length == 11
+            //                   select x;
 
+            //var queryResults = customers.Where(x => x.AccountNumber.Length == 11);
+            var queryResults = customers
+                .Where(x => x.AccountNumber.Length == 11)
+                .Select(elem => elem.Name);
+
+            Console.WriteLine();
             foreach (var x in queryResults)
             {
-                Console.Write(x + " ");
+                Console.WriteLine(x + " ");
             }
+
         }
 
         private static void LINQEgyszeruPelda()
@@ -41,6 +116,7 @@ namespace _2025_01_06_LINQ_alapok
 
             //var queryResults = egeszek.Where(szam => szam > 3);
             //var queryResults = egeszek.Where(szam => szam > 3).Select(szam=>szam+"#d");
+            //Lambda kifejezés
             List<int> queryResults = egeszek.Where(szam => szam > 3).ToList();
 
             foreach (var szam in queryResults)
@@ -57,11 +133,12 @@ namespace _2025_01_06_LINQ_alapok
 			
 			customers = new List<BankAccount>()
             {
-                new BankAccount("John Doe", 1000.0, "123-456-789"),
-                new BankAccount("Alice Smith", 0.0, "987-654-321"),
+                new BankAccount("John Doe", 1000.0, "123-456-789"), //M1
+                new BankAccount("Alice Smith", 0.0, "987-654-321"),//M2
                 new BankAccount("Robert Johnson", 1000000.0, "555-123-999"),
                 new BankAccount("Sarah White", 50.5, "666-777-888"),
-                new BankAccount("Superman", 5000.0, "000-000-001"),
+                new BankAccount("Superman", 5000.0, "000-000-001"),//M5
+                new BankAccount("Superman", 10000.0, "000-000-002"),//M6
                 new BankAccount("Bruce Wayne", 0.0, "111-222-333"),
                 new BankAccount("Peter Parker", 1200.75, "444-555-666"),
                 new BankAccount("Tony Stark", 99999999.99, "1234-5678-9012-3456")
---
+++ 2025_01_10_LinqValasztas/2025_01_10_LinqValasztas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_01_10_LinqValasztas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
