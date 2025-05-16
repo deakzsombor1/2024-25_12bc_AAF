ccbca42f57c1a2389d502a25db0ccdab0d285a39
Deák Zsombor <deak.zsombor@ckik.loc>
2025-05-16 09:34:08 +0200

    2025_05_16

--- .idozito/idozito/MainWindow.xaml.cs
+++ .idozito/idozito/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,57 +10,94 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace idozito
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        static int stopper_mero = 0;
-        static System.Timers.Timer timer = new System.Timers.Timer();
-        static bool fut_e = false;
+        private Stopwatch stopperOra = new Stopwatch();
+        private DispatcherTimer frissito = new DispatcherTimer();
+        private Stopwatch reszIdoStopper = new Stopwatch();
+        private bool fut_e = false;
 
         public MainWindow()
         {
             InitializeComponent();
-            timer.Interval = 10;
-            timer.Elapsed += OnTimerElapsed;
+
+            frissito.Interval = TimeSpan.FromMilliseconds(10);
+            frissito.Tick += Frissito_Tick;
         }
 
-        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        private void Frissito_Tick(object sender, EventArgs e)
         {
-            stopper_mero += 1;
-            stopper.Dispatcher.Invoke(() =>
-            {
-                int perc = stopper_mero / 6000;
-                int masodperc = (stopper_mero % 6000) / 100;
-                int szazadmp = stopper_mero % 100;
-                stopper.Text = string.Format("{0:D2}:{1:D2}.{2:D2}", perc, masodperc, szazadmp);
-            });
+            // Fo ido
+            var elapsed = stopperOra.Elapsed;
+
+            int perc = elapsed.Minutes;
+            int masodperc = elapsed.Seconds;
+            int szazadmp = elapsed.Milliseconds / 10;
+
+            stopper.Text = string.Format("{0:D2}:{1:D2}.{2:D2}", perc, masodperc, szazadmp);
+
+
+            // Resz ido
+            var resz = reszIdoStopper.Elapsed;
+            int rPerc = resz.Minutes;
+            int rMasodperc = resz.Seconds;
+            int rSzazad = resz.Milliseconds / 10;
+
+            reszido.Text = string.Format("{0:D2}:{1:D2}.{2:D2}", rPerc, rMasodperc, rSzazad);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (fut_e)
             {
-                timer.Stop();
-                fut_e= false;
+                reszIdoStopper.Stop();
+                stopperOra.Stop();
+                frissito.Stop();
+                fut_e = false;
                 indito_megallito.Content = "Folytatás";
                 indito_megallito.Background = Brushes.LightGray;
+                kor.Content = "Törlés";
+                kor.Background = Brushes.IndianRed;
             }
-            else {
-                timer.Start();
+            else
+            {
+                reszIdoStopper.Start();
+                stopperOra.Start();
+                frissito.Start();
                 fut_e = true;
                 indito_megallito.Content = "Leállít";
                 indito_megallito.Background = Brushes.IndianRed;
+                kor.Content = "Kör";
+                kor.Background = Brushes.LightGreen;
             }
         }
 
         private void kor_Click(object sender, RoutedEventArgs e)
         {
+            if (fut_e)
+            {
 
+                reszIdoStopper.Restart();
+                string aktualisIdo = stopper.Text;
+                string reszIdo = reszido.Text;
+                listBox.Items.Insert(0, reszIdo);
+
+            }
+            else
+            {
+                reszIdoStopper.Reset();
+                reszido.Text = "00:00.00";
+                stopperOra.Reset();
+                stopper.Text = "00:00.00";
+                listBox.Items.Clear();
+                kor.Content = "Kör";
+                kor.Background = Brushes.LightGray;
+                indito_megallito.Content = "Indítás";
+            }
         }
     }
 }
